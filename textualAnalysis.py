d2da0084585153287daea9a1dfbd3519df34d5e2
Drew Beckmen <dbeckmen2019@pingry.org>
2018-05-27 18:32:18 -0400

    Added additional functionality for more data.

--- textualAnalysis.py
+++ textualAnalysis.py
@@ -13,24 +13,85 @@ from collections import Counter
 f = open('Extremely Loud And Incredibly Close -Jonathan Safran_djvu.txt', 'r')
 fileLines = f.read().lower()
 frequency = {}
+entireFrequency = {}
+
+s = open('1-1000.txt', 'r')
+readLines = s.readlines()
+
+animals = open('Animal-words.txt', 'r')
+animalList = animals.readlines()
+
+#Get rid of newline
+for i in range(len(readLines)):
+    readLines[i] = readLines[i].strip('\n')
+
+for i in range(len(animalList)):
+    animalList[i] = animalList[i].strip('\n').lower()
 
 for word in fileLines.split():
-    if word not in frequency:
-        frequency[word] = 1
+    if word not in readLines:
+        if word not in frequency:
+            frequency[word] = 1
+        else:
+            frequency[word] += 1
+
+for word in fileLines.split():
+    if word not in entireFrequency:
+        entireFrequency[word] = 1
     else:
-        frequency[word] += 1
+        entireFrequency[word] += 1
+
+#Get the top 5 most used words outside of the 250 most commonly used words
+top5 = dict(Counter(frequency).most_common(25))
+print(top5)
 
 #Find the frequency of each of these words in the entire novel
-print("Number of times extremely is said: {}".format(frequency["extremely"]))
-print("Number of times incredibly is said: {}".format(frequency["incredibly"]))
-print("Number of times close is said: {}".format(frequency["close"] + frequency["closely"]))
-print("Number of times loud is said: {}".format(frequency["loud"] + frequency["loudly"]))
+print("Number of times extremely is said: {}".format(entireFrequency["extremely"]))
+print("Number of times incredibly is said: {}".format(entireFrequency["incredibly"]))
+print("Number of times close is said: {}".format(entireFrequency["close"] + entireFrequency["closely"]))
+print("Number of times loud is said: {}".format(entireFrequency["loud"] + entireFrequency["loudly"]))
 
-#Other things to look for -->
+
+
+
+#Look for colors in the book
+colors = [" blue ", " red ", " orange ", " yellow ", " green ", " white ", " brown ", " purple ", " violet "]
+timesMentioned = 0
+for key in colors:
+    timesMentioned += fileLines.count(key)
+    print("Number of times {} is said: {}".format(key, fileLines.count(key)))
+
+print("Colors are brought up {} times.".format(timesMentioned))
+
+#Look for animals in the book
+animalsMentioned = 0;
+animalsMentioned += entireFrequency["animal"]
+for animal in animalList:
+    if animal in frequency:
+        animalsMentioned += entireFrequency[animal]
+
+print("Animals are brought up {} times".format(animalsMentioned))
+
+
+def testFloat(value):
+    try:
+        int(value)
+        return True
+    except ValueError:
+        return False
+
+numbers = 0
+for key in entireFrequency:
+    if testFloat(key):
+        numbers += 1
+
+print("Numbers are brought up {} times".format(numbers))
 
 f.close()
-#-------------------------------------------------------------------------------
+s.close()
 
+
+#-------------------------------------------------------------------------------
 
 
 #-------------------------ANALYZE OSKAR'S NARRATION-----------------------------
